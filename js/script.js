0def8e42eaa6d06ff2b9a8f07035ca4b81d66e3d
Silvar925 <silvarmoonlight@gmail.com>
2024-01-12 12:42:28 +0300

    V1.0.3

--- js/script.js
+++ js/script.js
@@ -1,26 +1,5 @@
-const header = document.getElementById('header')
-const nav = document.createElement('nav')
+const listFriendContainer = document.getElementById('profile_section_container_friends')
+const messengerContainer = document.getElementById('profile_section_container_messenger')
 
-nav.innerHTML = `<h1>Александрия</h1>
-<ul>
-    <li><a href="#">Главная</a></li>
-    <li><a href="#">Тренажер</a></li>
-    <li><a href="#">Магазин</a></li>
-    <li><a href="#">О нас</a></li>
-</ul>
-
-<div class="profile_container">
-    <div class="profile_container_img">
-        <img src="images/av4.png" alt="#">
-    </div>
-
-    <div class="profile_container_active">
-        <p>Хатидже Сафина</p>
-        <select name="" id="">
-            <option value="">Профиль</option>
-            <option value="">Выйти</option>
-        </select>
-    </div>
-</div>`
-
-header.appendChild(nav)
+const btnMessenger = document.getElementById('btnMessenger')
+const btnFriends = document.getElementById('btnFriends')
